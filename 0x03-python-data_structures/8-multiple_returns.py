d53259192d9a5de4155dfe418b89c036901307f7
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-11-05 14:43:11 -0800

    add tuple keyword

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentance) > 0:
-        return (len(sentance), sentance[0])
+        return tuple(len(sentance), sentance[0])
     else:
-        return (len(sentance), None)
+        return tuple(len(sentance), None)
