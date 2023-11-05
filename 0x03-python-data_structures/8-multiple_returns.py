b895bf8d2cb61f6549cd314519a7b5fa2a10fd75
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-11-05 14:41:49 -0800

    modify the condition

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentance) == 0:
+    if len(sentance) > 0:
+        return (len(sentance), sentance[0])
+    else:
         return (len(sentance), None)
-    return (len(sentance), sentance[0])
