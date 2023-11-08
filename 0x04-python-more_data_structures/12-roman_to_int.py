9906be5b6c4448f67f8c9787cdaabb7a4569c539
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-11-08 15:19:45 -0800

    Roman to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    from functools import reduce
+    roman_numeral = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    return reduce((lambda x, y: x + y if x >= y else y - x), [roman_numeral[c] for c in roman_string])
