1469336a41572ab6a0c9295e02b35ea2a7a499a9
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-11-08 14:26:36 -0800

    remove test code

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,13 +3,3 @@ def multiply_by_2(a_dictionary):
     new_dict = a_dictionary.copy()
     new_dict.update((key, value * 2) for key, value in new_dict.items())
     return new_dict
-
-def print_sorted_dictionary(a_dictionary):
-    for key, value in sorted(a_dictionary.items()):
-        print("{:s}: {}".format(key, value))
-
-a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
-new_dict = multiply_by_2(a_dictionary)
-print_sorted_dictionary(a_dictionary)
-print("--")
-print_sorted_dictionary(new_dict)
