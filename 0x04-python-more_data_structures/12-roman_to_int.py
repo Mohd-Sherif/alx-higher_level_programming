f779801680d5aed9d766537b19ca42f23b10c0a5
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-11-08 15:26:15 -0800

    import before the function

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+from functools import reduce
+
 def roman_to_int(roman_string):
-    from functools import reduce
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
     roman_numeral = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
