25aea9af570eedde1542a16fae9ad7ab92830680
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-11-09 12:49:36 -0800

    replace using reduce function by using for loop

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,5 +3,23 @@ def roman_to_int(roman_string):
     from functools import reduce
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    roman_numeral = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000, 'i': 1, 'v': 1, 'x': 10, 'l': 50, 'c': 100, 'd': 500, 'm': 100}
-    return reduce((lambda x, y: x + y if x >= y else y - x), [roman_numeral[c] for c in roman_string])
+    roman_numeral = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+            }
+#    return reduce((lambda x, y: x + y if x >= y else y - x), [roman_numeral[c] for c in roman_string])
+    result = 0
+    prev_value = 0
+    for c in roman_string:
+        value = roman_numeral[c]
+        if value > prev_value:
+            result += value - 2 * prev_value
+        else:
+            result += value
+        prev_value = value
+    return result
