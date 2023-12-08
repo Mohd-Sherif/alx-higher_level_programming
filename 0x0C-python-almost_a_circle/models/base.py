e54a142a75fdc1b89532b9a655c1f11729dc159f
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-12-08 07:04:48 -0800

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module for Base class"""
+
+
+class Base:
+    """
+    Class that defines properties of Base.
+
+     Attributes:
+        id (int): Identity of each instance.
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Creates new instances of Base.
+
+        Args:
+            id (int): Identity of each instance. Defaults to None.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
