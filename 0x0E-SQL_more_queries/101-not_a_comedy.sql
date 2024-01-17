1fc577bb66d348304761c3fdce70b947d88d69cd
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-01-17 15:01:08 -0800

    No Comedy tonight!

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,11 @@
+-- a script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
+SELECT title
+FROM tv_shows
+WHERE id NOT IN (
+	SELECT ts.id
+	FROM tv_genres AS tg
+	INNER JOIN tv_show_genres tsg ON tg.id = tsg.genre_id
+	INNER JOIN tv_shows ts ON tsg.show_id = ts.id
+	WHERE tg.name = 'Comedy'
+	)
+ORDER BY title;
