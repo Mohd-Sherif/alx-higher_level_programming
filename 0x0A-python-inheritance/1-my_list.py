cfbc9560eae010db0454e1e0800821149215a8db
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-02-09 06:27:57 -0800

    remove __init__ method

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,10 +5,6 @@
 class MyList(list):
     """Class that defines properties of MyList."""
 
-    def __init__(self):
-        """Creates new instances of MyList."""
-        super()
-
     def print_sorted(self):
         """Prints the list, but sorted (ascending sort)."""
         print(sorted(self))
