bd5b62a1e16c90f01f536d74d13055270398075d
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-02-10 03:55:57 -0800

    Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for write_file method"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8) and returns the number of characters written"""
+    with open(filename, 'w') as f:
+        return f.write(text)
