e0f049bcc56c15c0f936f4ea24961c12f16f44e4
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-02-10 03:48:00 -0800

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for read_file method"""
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, 'r') as f:
+        print(f.read(), end="")
