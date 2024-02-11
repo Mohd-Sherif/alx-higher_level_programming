1c9e9c1ffd9f4274bb96c81dd3c90339a26360d7
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-02-11 04:56:23 -0800

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module Student Class"""
+
+
+class Student:
+    """Define Student Class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        json_dict = {}
+        for attr_name in attrs:
+            if isinstance(attr_name, (str, int)) and hasattr(self, attr_name):
+                json_dict[attr_name] = getattr(self, attr_name)
+        return json_dict
