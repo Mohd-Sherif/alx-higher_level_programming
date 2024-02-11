7369edb899357655c46f2d68158d508b0ab1edd1
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-02-11 05:06:47 -0800

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module Student Class"""
+
+
+class Student:
+    """Define Student Class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        json_dict = {}
+        for attr_name in attrs:
+            if isinstance(attr_name, (str, int)) and hasattr(self, attr_name):
+                json_dict[attr_name] = getattr(self, attr_name)
+        return json_dict
+
+    def reload_from_json(self, json):
+        self.first_name = json.attr_name.first_name
+        self.last_name = json.attr_name.last_name
+        self.age = json.attr_name.age
