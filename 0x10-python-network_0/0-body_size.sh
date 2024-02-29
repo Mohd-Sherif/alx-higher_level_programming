bbf7a08043f3061daa92a348fad1d7b32ce24c93
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-02-29 11:49:51 -0800

    one line script

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
-URL=$1
-
-curl -s "$URL" | wc -c
+curl -s "$1" | wc -c
