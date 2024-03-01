d2fe616d0a40dc781410fb9eb634b9527bea0166
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-03-01 01:08:06 -0800

    add tab spaces

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     with req.urlopen('https://alx-intranet.hbtn.io/status') as res:
         body = res.read()
     print('Body response:')
-    print('    - type: {}'.format(type(body)))
-    print('    - content: {}'.format(body))
-    print('    - utf8 content: {}'.format(body.decode()))
+    print('\t- type: {}'.format(type(body)))
+    print('\t- content: {}'.format(body))
+    print('\t- utf8 content: {}'.format(body.decode()))
