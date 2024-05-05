5c07319d6a7c4973cca2afdfe46093d69acca000
Mohamed Sherif <111144120+Mohd-Sherif@users.noreply.github.com>
2024-05-05 13:18:01 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/1-multi_languages.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const myVarC = 'C is fun';
-const myVarPy = 'Python is cool';
-const myVarJS = 'JavaScript is amazing';
-
-console.log(myVarC);
-console.log(myVarPy);
-console.log(myVarJS);
