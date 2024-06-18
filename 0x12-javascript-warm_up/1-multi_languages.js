0b05dea1bd9e8300a8ccb3c243db1feb5715039d
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-06-18 05:14:23 -0700

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const myVarC = 'C is fun';
+const myVarPy = 'Python is cool';
+const myVarJS = 'JavaScript is amazing';
+
+console.log(myVarC);
+console.log(myVarPy);
+console.log(myVarJS);
