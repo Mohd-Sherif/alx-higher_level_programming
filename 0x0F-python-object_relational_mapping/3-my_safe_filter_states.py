9a1b76ab2dcecd971b116e2bff84b12c387fae55
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-06-21 06:10:17 -0700

    SQL Injection...

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument
+But this time, it is safe from MySQL injections!
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    host = 'localhost'
+    port = 3306
+    dbUsr = sys.argv[1]
+    dbPass = sys.argv[2]
+    dbName = sys.argv[3]
+    stateNameSearched = sys.argv[4]
+
+    db = MySQLdb.connect(
+            host=host,
+            port=port,
+            user=dbUsr,
+            passwd=dbPass,
+            db=dbName
+        )
+    cur = db.cursor()
+
+    query = \
+        """
+        SELECT *
+        FROM states
+        WHERE name=%s
+        ORDER BY id;
+        """
+    cur.execute(query, (stateNameSearched,))
+
+    res = cur.fetchall()
+
+    for row in res:
+        print(row)
+
+    cur.close()
+    db.close()
