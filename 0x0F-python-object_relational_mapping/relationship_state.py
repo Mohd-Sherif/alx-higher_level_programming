3ad180f961c83c98e31c67580b5608fa03617c7c
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-06-21 06:41:00 -0700

    City relationship

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module State Class"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Define State Class"""
+    __tablename__ = 'states'
+
+    id = Column(
+            Integer,
+            autoincrement=True,
+            nullable=False,
+            primary_key=True
+        )
+    name = Column(
+            String(128),
+            nullable=False
+        )
+    cities = relationship(
+            "City",
+            cascade="all, delete-orphan",
+            backref="state"
+        )
