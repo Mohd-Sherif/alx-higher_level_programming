a7c1f70b4d47a05c114d8779a56036a719401bba
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-07-05 00:52:57 -0700

    pycodestyle correction

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,14 +7,15 @@ def find_peak(list_of_integers):
 
     if list_of_integers == []:
         return None
-    l = len(list_of_integers)
+    length = len(list_of_integers)
     mid = int(l / 2)
-    if l == 1:
+
+    if length == 1:
         return list_of_integers[0]
-    if l == 2:
+    if length == 2:
         return max(list_of_integers[0], list_of_integers[1])
     if list_of_integers[mid - 1] <= list_of_integers[mid] and\
-          list_of_integers[mid + 1] <= list_of_integers[mid]:
+            list_of_integers[mid + 1] <= list_of_integers[mid]:
         return list_of_integers[mid]
     if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
         return find_peak(list_of_integers[mid:])
